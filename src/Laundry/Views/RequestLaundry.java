337ada6c12b3286d61e02e6e0bf648d2cefcc597
maulanaakurniaa@yahoo.com <maulanaakurniaa@yahoo.com>
2020-05-27 17:42:48 +0800

    Change view and add insert request laundry user on admin

--- src/Laundry/Views/RequestLaundry.java
+++ src/Laundry/Views/RequestLaundry.java
@@ -19,6 +19,7 @@ import java.util.Locale;
 public class RequestLaundry extends JFrame {
     JFrame window = new JFrame("Request Laundry");
     // JLabel
+    JLabel lUser        = new JLabel("User");
     JLabel lBerat       = new JLabel("Berat");
     JLabel lkg          = new JLabel("Kg");
     JLabel lJenis       = new JLabel("Jenis Cucian");
@@ -51,58 +52,88 @@ public class RequestLaundry extends JFrame {
     static int harga;
     static float berat, total;
 
-    public RequestLaundry(){
-        initComponents();
-        loadJenisCucian();
-        initListeners();
+    public RequestLaundry() {
+        if (UserSession.getId_user() == null) {
+            JOptionPane.showMessageDialog(null, "Silahkan login terlebih dahulu!", "Peringatan", JOptionPane.WARNING_MESSAGE);
+            window.setVisible(false);
+            new Login();
+        } else{
+            getAllUser();
+            initComponents();
+            loadJenisCucian();
+            initListeners();
+        }
     }
 
     private void initComponents(){
         window.getContentPane().setBackground(new Color(28, 27, 27));
+        if(UserSession.getId_user().equals("1")) {
+            window.add(lUser);
+                lUser.setBounds(40,20,150,20);
+                lUser.setForeground(new Color(255, 255, 255));
+            window.add(cUser);
+                cUser.setBounds(150,20,170,25);
 
+            lBerat.setBounds(40, 63, 150, 20);
+            lkg.setBounds(335, 63, 150, 20);
+            fBerat.setBounds(150, 60, 170, 25);
+            cJenis.setBounds(150, 100, 170, 25);
+            lJenis.setBounds(40, 103, 150, 20);
+            lDurasi.setBounds(40, 143, 150, 20);
+            fDurasi.setBounds(150, 140, 170, 25);
+            lHarga.setBounds(40, 183, 150, 20);
+            fHarga.setBounds(150, 180, 170, 25);
+            lTotal.setBounds(40, 223, 150, 20);
+            fTotal.setBounds(150, 220, 170, 25);
+            bLanjut.setBounds(150,265,170,25);
+            bKembali.setBounds(40,265,90,25);
+            window.setSize(375, 360);
+
+        }else{
+            lBerat.setBounds(40, 23, 150, 20);
+            lkg.setBounds(335, 23, 150, 20);
+            fBerat.setBounds(150, 20, 170, 25);
+            lJenis.setBounds(40, 63, 150, 20);
+            cJenis.setBounds(150, 60, 170, 25);
+            lDurasi.setBounds(40, 103, 150, 20);
+            fDurasi.setBounds(150, 100, 170, 25);
+            lHarga.setBounds(40, 143, 150, 20);
+            fHarga.setBounds(150, 140, 170, 25);
+            lTotal.setBounds(40, 183, 150, 20);
+            fTotal.setBounds(150, 180, 170, 25);
+            bLanjut.setBounds(150,220,170,25);
+            bKembali.setBounds(40,220,90,25);
+            window.setSize(375, 320);
+        }
         window.add(lBerat);
-            lBerat.setBounds(20, 20, 150, 20);
             lBerat.setForeground(new Color(255, 255, 255));
             window.add(lkg);
-            lkg.setBounds(315, 20, 150, 20);
             lkg.setForeground(new Color(255, 255, 255));
             window.add(fBerat);
-                fBerat.setBounds(130, 20, 170, 25);
 
         window.add(lJenis);
-            lJenis.setBounds(20, 63, 150, 20);
             lJenis.setForeground(new Color(255, 255, 255));
             window.add(cJenis);
-                cJenis.setBounds(130, 60, 170, 25);
 
         window.add(lDurasi);
-            lDurasi.setBounds(20, 103, 150, 20);
             lDurasi.setForeground(new Color(255, 255, 255));
             window.add(fDurasi);
-                fDurasi.setBounds(130, 100, 170, 25);
                 fDurasi.setEditable(false);
 
         window.add(lHarga);
-            lHarga.setBounds(20, 143, 150, 20);
             lHarga.setForeground(new Color(255, 255, 255));
             window.add(fHarga);
-                fHarga.setBounds(130, 140, 170, 25);
                 fHarga.setEditable(false);
 
         window.add(lTotal);
-            lTotal.setBounds(20, 183, 150, 20);
             lTotal.setForeground(new Color(255, 255, 255));
             window.add(fTotal);
-                fTotal.setBounds(130, 180, 170, 25);
                 fTotal.setEditable(false);
 
         window.add(bLanjut);
-            bLanjut.setBounds(130,220,170,25);
         window.add(bKembali);
-            bKembali.setBounds(20,220,90,25);
 
         window.setLayout(null);
-        window.setSize(365, 300);
         window.setVisible(true);
         window.setLocationRelativeTo(null);
         window.setResizable(false);
@@ -149,7 +180,7 @@ public class RequestLaundry extends JFrame {
                 total    = berat*harga;
                 fTotal.setText("Rp."+ nf.format(total));
             }catch(NumberFormatException ne){
-                JOptionPane.showMessageDialog(null, "Berat Harus Diisi!");
+                JOptionPane.showMessageDialog(null, "Nilai berat tidak valid!");
                 fDurasi.setText("");
                 fHarga.setText("");
                 fTotal.setText("");
@@ -193,8 +224,17 @@ public class RequestLaundry extends JFrame {
                         String pmsn = "PMSN-";
                         kode = pmsn + String.format("%03d", no_urut);
                     }
-                    statement.executeUpdate("INSERT INTO pemesanan VALUES('" + kode + "','" + UserSession.getId_user() + "','" + idCucian + "','"+ berat +"','" + total + "','"+ time.format(now) +"','"+time.format(now)+"','SEDANG DIPROSES')");
-                    pesan = true;
+                    if(UserSession.getId_user().equals("1")){
+                        String sql = "SELECT id_user,username FROM user WHERE username='"+cUser.getSelectedItem()+"'";
+                        resultSet  = statement.executeQuery(sql);
+                        resultSet.next();
+                        String idUser = resultSet.getString("id_user");
+                        statement.executeUpdate("INSERT INTO pemesanan VALUES('" + kode + "','" + idUser + "','" + idCucian + "','" + berat + "','" + total + "','" + time.format(now) + "','" + time.format(now) + "','MENUNGGU DIKONFIRMASI')");
+                        pesan = true;
+                    }else {
+                        statement.executeUpdate("INSERT INTO pemesanan VALUES('" + kode + "','" + UserSession.getId_user() + "','" + idCucian + "','" + berat + "','" + total + "','" + time.format(now) + "','" + time.format(now) + "','MENUNGGU DIKONFIRMASI')");
+                        pesan = true;
+                    }
                 }
 
             resultSet.close();
@@ -205,6 +245,21 @@ public class RequestLaundry extends JFrame {
             JOptionPane.showMessageDialog(rootPane, "Driver tidak ditemukan !!");
         }catch (NumberFormatException e){
             System.err.println("error"+e);
+        }
+    }
+
+    private void getAllUser(){
+        try {
+            statement = koneksi.getConnection().createStatement();
+            String sql = "SELECT * FROM user";
+            resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                cUser.addItem(resultSet.getString("username"));
+            }
+        } catch (SQLException sqlError) {
+            JOptionPane.showMessageDialog(null, "Data Gagal Ditampilkan" + sqlError);
+        } catch (ClassNotFoundException classError) {
+            JOptionPane.showMessageDialog(null, "Driver tidak ditemukan !!");
         }
     }
 
