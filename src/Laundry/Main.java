6ed6c9462a8727dcda2851daad8ba719ef6719f5
Maulana Kurnia <50382874+maulanakurnia@users.noreply.github.com>
2020-05-28 17:47:19 +0800

    Main

--- src/Laundry/Main.java
+++ src/Laundry/Main.java
@@ -1,11 +1,13 @@
 package Laundry;
 
+import Laundry.Views.KelolaUser;
 import Laundry.Views.Login;
 import Laundry.Views.Pesanan;
+import Laundry.Views.RequestLaundry;
 
 public class Main {
 
     public static void main(String[] args) {
-        new Pesanan();
+        new Login();
     }
 }
